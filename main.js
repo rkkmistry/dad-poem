af60b77a4c98ee176b4a06a4e5054013278e67ab
rkkmistry <rkkmistry@gmail.com>
2017-05-11 18:40:06 -0400

    trying to get sound to work on mobile

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 text = [ "it’s just struck me", "that the fondest memories", "I have of my dad", "are memories", "of being sick as a child", "taken out of school at lunch", "bundled up in a sweater", "on the couch at midday", "his hands coaxing a warm drink ", "down my sore throat", "I’m struck", "by the weight of these memories", "the fondness I feel towards", "these moments", "that are so inconsequential", "yet so deeply memorable", "they carry a weight", "a mood and atmosphere", "something you can look back on", "but also pause for a moment", "and feel ", "immersed in the warm nectar of care", "slipping in and out of faint sleep", "the touch of soft blankets", "and the weight of a gentle hand ", "sick and being cared for ", "by my dad", "no one else there", "when my body couldn’t lie", "And my father could see", "I was ailing", "a moment of divine soundless communication", "a brief space of total acceptance", "A single point of understanding", "a father and his son", "one sitting and one lying on a bed", "both wrapped in mutual sincerity", "a quiet and soundless moment", "simply a man caring for his child", "one creature caring for another", "the trappings of human life seem to fall away", "in these moments", "I felt connected", "to this human who had raised me", "by a heartfelt desire", "and I would slowly fall into a tender sleep", "Knowing that I could rest easy", "Under the kind eye", "of my father" ];
 
+//sound 
+var snowstorm = new Audio('snowstorm.mp3');
+var avril = new Audio('avril.mp3');
+snowstorm.play();
+
 //this is mostly stolen from @toddword's "in the dark" project 
 var count = 0;
 var notStarted = true;
 Mousetrap.bind('space', function() {
   if (notStarted) {
     setTimeout(function(){
-      $('#audio')[0].play();
+      avril.play();
       placeText();
     }, 1000);
     $('header').fadeOut(1000);
@@ -22,6 +27,7 @@ function touch() {
   if (notStarted) {
     setTimeout(function(){
       $('#audio')[0].play();
+      $('#bg-audio')[0].play();
       placeText();
     }, 1000);
     $('header').fadeOut(1000);
